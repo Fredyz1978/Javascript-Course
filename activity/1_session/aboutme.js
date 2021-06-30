b6707512d0bb32a197d9527b574dff3c956a89a9
Fredy Zurita <Fredy Zurita@PC014>
2021-06-29 20:51:39 -0500

    actividad 1 finalizada

---
+++ activity/1_session/aboutme.js
@@ -0,0 +1,5 @@
+let nombrecompleto = "Fredy Zurita";
+let profesion="Master Developer";
+let cargoaspirado="Arquitecto de Sofware";
+
+console.log(`Hola, me llamo ${nombrecompleto} y me dedico a ${profesion}. Estoy cursando este Máster porque me gustaría trabajar en ${cargoaspirado}`);
--- activity/1_session/index.js
+++ activity/1_session/index.js
@@ -1,5 +1,7 @@
 // Ejercicio 1
 
+import {} from "./aboutme.js";
+
 console.log("Ejercicio 1");
 
 // Ejercicio 2
@@ -7,8 +9,19 @@ console.log("Ejercicio 1");
 // Ejercicio 3
 
 // Ejercicio 4
+for(var i=1;i<100;i++){
+    if(i%7==0)
+        console.log(i);
+}
 
 // Ejercicio 5
 
 // Ejercicio 6
+let formatter={prefix:"Hola", 
+    append: function(param){console.log(formatter.prefix+" "+param);},
+    toLowerString: function(parameter){console.log(parameter.toLowerCase());}
+};
 
+formatter.append("World");
+
+formatter.toLowerString("I´m Fredy Zurita");
